7cf19ca17fbd904735c2a612abaddcd611447b73
henokwel <henokwel@outlook.com>
2022-01-20 22:55:54 +0100

    testing .env for nextjs

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  console.log(process.env.MY_KEY);
+  
   return (
     <div className={styles.container}>
       <Head>
